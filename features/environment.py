bfecb9606a434b459d1126ea6b6e48ba6b27676a
Daniel Davis <daniel.davis@excella.com>
2016-08-03 08:24:32 -0400

    Adding selenium tests to the mix

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,20 @@
-def before_all(context):
-    pass
+from selenium import webdriver
+import time
 
-def before_scenario(context, scenario):
-    pass
+# def before_all(context):
+#     context.driver = webdriver.Chrome('./vendor/chromedriver')
+#     context.driver.get('localhost:8000')
+#     time.sleep(5)
 
-def after_scenario(context, scenario):
-    pass
+# def before_scenario(context, scenario):
+#     pass
+
+# def before_step(context, scenario):
+#     time.sleep(2)
+
+# def after_scenario(context, scenario):
+#     pass
+
+# def after_all(context):
+#     time.sleep(5)
+#     context.driver.quit()
--- features/steps/common.py
+++ features/steps/common.py
@@ -1,6 +1,7 @@
 from behave import *
 from django.core.urlresolvers import reverse
 from nose.tools import *
+import time
 
 @given("I have inserted a quarter")
 def insert_quarter(context):
@@ -13,3 +14,28 @@ def buy_a_product(context):
 @then("I should receive the product")
 def assert_recieve_product(context):
     assert_is_not_none(context.response.json()['product'])
+
+
+
+
+
+# @given("I have inserted a quarter")
+# def insert_quarter(context):
+#     insert_button = context.driver.find_element_by_id('insert-btn')
+#     insert_button.click()
+
+#     # time.sleep(0.5)
+#     # insert_button.click()
+#     # time.sleep(0.5)
+#     # insert_button.click()
+
+# @when("I purchase a product")
+# def buy_a_product(context):
+#     buy_button = context.driver.find_element_by_id('buy-btn')
+#     buy_button.click()
+
+# @then("I should receive the product")
+# def assert_recieve_product(context):
+#     message_element = context.driver.find_element_by_id('msg')
+#     assert_equals(message_element.text, 'Enjoy!')
+
